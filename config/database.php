15d4a70a9ab692b4f7f660604de5a03ee273be0f
Habib Frambudi <59114869+frambudi75@users.noreply.github.com>
2026-09-15 02:41:22 +0700

    delete enkripsi

--- config/database.php
+++ config/database.php
@@ -17,10 +17,3 @@ defined('DB_CHARSET') or define('DB_CHARSET', 'utf8mb4');
 
 
 
-
-// defined('DB_HOST') or define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-// defined('DB_PORT') or define('DB_PORT', getenv('DB_PORT') ?: '3306');
-// defined('DB_NAME') or define('DB_NAME', getenv('DB_NAME') ?: 'diarynot_link');
-// defined('DB_USER') or define('DB_USER', getenv('DB_USER') ?: 'diarynot_link');
-// defined('DB_PASS') or define('DB_PASS', getenv('DB_PASS') !== false ? getenv('DB_PASS') : 'B2Gvh8TSr5Akwk3AFfvt');
-// defined('DB_CHARSET') or define('DB_CHARSET', 'utf8mb4');
